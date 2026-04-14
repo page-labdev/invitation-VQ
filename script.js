46d32ea3c9d2b13f0cd7e0cfe5fd85380b8a4303
page-labdev <nonaghazaryan97@gmail.com>
2026-04-14 14:02:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const envelope = document.getElementById('envelope');
 
+envelope.onclick = () => {
+  envelope.classList.add('open');
+
+  setTimeout(()=>{
+    document.querySelector('.content').classList.add('show');
+  },800);
+};
